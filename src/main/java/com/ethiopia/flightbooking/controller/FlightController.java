05443ad1383476a228904c69a2ca9b52651f73e4
dave <dawit.girma@d>
2019-10-15 21:50:12 +0300

    Final Skeleton

--- src/main/java/com/ethiopia/flightbooking/controller/FlightController.java
+++ src/main/java/com/ethiopia/flightbooking/controller/FlightController.java
@@ -1,17 +1,12 @@
 package com.ethiopia.flightbooking.controller;
 
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FlightController
 {
 
-    @GetMapping(value = {"/","/flightbooking", "/flightbooking/home"})
-    public String home()
-    {
-        return "index";
-    }
 
 
 }
---
+++ src/main/java/com/ethiopia/flightbooking/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.ethiopia.flightbooking.controller;
+
+import com.ethiopia.flightbooking.model.Flight;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController
+{
+    @GetMapping(value = {"/","/flightbooking", "/flightbooking/home"})
+    public ModelAndView home()
+    {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("flight",new Flight());
+        mav.setViewName("index");
+        return mav;
+    }
+
+}
