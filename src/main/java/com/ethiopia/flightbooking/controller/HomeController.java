c7daf97e709869f0758d7db4091a1c26f2c44fc8
Zelalem Zergaw <zola145103@gmail.com>
2019-10-21 15:35:25 -0500

    updated

--- src/main/java/com/ethiopia/flightbooking/controller/HomeController.java
+++ src/main/java/com/ethiopia/flightbooking/controller/HomeController.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController
 {
-    @GetMapping(value = {"/flightbooking", "/flightbooking/home",""})
+    @GetMapping(value = {"/","/flightbooking", "/flightbooking/home"})
     public ModelAndView home()
     {
         ModelAndView mav = new ModelAndView();
         mav.addObject("flight",new Flight());
         mav.setViewName("index");
         return mav;
-                }
+    }
 
-                }
+}
